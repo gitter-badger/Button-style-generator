4741eb5fee6b718d72b52e92f48b08c80ce8c9df
Tanotify <toropanov@gmail.com>
2014-08-27 12:05:35 +0400

    Gulp already configured. There are tasks for it: minifying css and js files from dev folder to public

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+
+    concatJS = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+
+    minifyCSS = require('gulp-minify-css'),
+    concatCSS = require('gulp-concat-css'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass');
+
+gulp.task('styles', function () {
+    gulp.src('./dev/styles/*.scss')
+        .pipe(sass())
+        .pipe(concatCSS("styles.css"))
+        .pipe(minifyCSS({keepBreaks:false}))
+        .pipe(rename({suffix:".min"}))
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('scripts', function () {
+    gulp.src('./dev/js/*.js')
+        .pipe(concatJS("scripts.js"))
+        .pipe(uglify())
+        .pipe(rename({suffix:".min"}))
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('default', ['styles', 'scripts']);
