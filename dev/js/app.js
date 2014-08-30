11d9510854515741fa9ba0adf845f78645586e81
Anton <tanotify@Antons-MacBook-Pro.local>
2014-08-30 18:45:39 +0400

    Started to work with button style generator. But there are still a lot of work with it.

--- dev/js/app.js
+++ dev/js/app.js
@@ -2,6 +2,27 @@ angular.module('app', ['control']);
 
 var control = angular.module('control', []);
 
-control.controller('GreetingController', function ($scope) {
-    $scope.greeting = 'Hola!';
+control.controller('generatorOptions', function ($scope) {
+
+    console.log($scope.buttonStyle);
+
+    $scope.buttonStyle = {
+        "width" : "900px",
+        "background" : "red"
+    };
+
 });
+
+control.controller('generatorResult', function ($scope) {
+//    $scope.greeting = 'Hola!';
+
+    $scope.buttonStyle = 'Hol3a';
+
+});
+
+
+//<div ng-style="{ 'width' : width, 'background' : bgColor }"></div>
+//And then in your controller:
+//
+//    $scope.width = '900px';
+//$scope.bgColor = 'red';
