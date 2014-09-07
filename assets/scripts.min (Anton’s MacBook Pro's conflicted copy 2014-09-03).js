fb04cf1c963a7da0fc14317de7768b1555a10195
Tanotify <tanotify@gmail.com>
2014-09-07 19:50:45 +0400

    Now pages working well.

---
+++ assets/scripts.min (Anton’s MacBook Pro's conflicted copy 2014-09-03).js
@@ -0,0 +1,18 @@
+angular.module('app', ['control']);
+
+var control = angular.module('control', []);
+
+control.controller('generatorOptions', function ($http, $scope) {
+
+    $http.get('buttons.json')
+        .then(function(res){
+            $scope.buttonStyles = res.data;
+    });
+
+});
+
+control.directive('options', function() {
+    return {
+        templateUrl: 'views/options.html'
+    };
+});
