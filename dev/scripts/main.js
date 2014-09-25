5801c7d2f097207bcfbb3e2ba73a88e75d7ccb1c
Tanotify <toropanov@gmail.com>
2014-09-25 16:42:39 +0400

    Added WebStorm Git integration Finished main part

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -3,13 +3,15 @@ $(window).ready(function(){
 
         init: function () {
             this.listen();
-            //this.updateCSSResult();
+            this.updateCSSResult();
+            this.updateHtmlResult();
         },
 
         listen: function () {
             $('#border-radius').on('change', $.proxy(this.changeBorderRadius, this));
-            $('#border-size').on('change', $.proxy(this.changeBorderSize, this));
+            $('#border-width').on('change', $.proxy(this.changeBorderWidth, this));
             $('#button-text').on('keyup', $.proxy(this.changeText, this));
+            $('#send-form').on('click', app.sendForm);
         },
 
         preview: $('.button-preview'),
@@ -17,33 +19,81 @@ $(window).ready(function(){
         //Changing border-radius
         changeBorderRadius: function() {
             var newBorderRadius = $('#border-radius').val();
-            this.preview.css({
-                'border-radius': newBorderRadius
-            });
-            //this.updateCSSResult();
+            this.preview.css(
+                'border-radius', newBorderRadius + 'px'
+            );
+            this.updateCSSResult();
         },
 
         //Changing border-size
-      changeBorderSize: function() {
-            var newBorderSize = $('#border-size').val();
-            this.preview.css({
-              'border-size': newBorderSize
-            });
-            //this.updateCSSResult();
+      changeBorderWidth: function() {
+            var newBorderWidth = $('#border-width').val();
+          this.preview.css(
+              'border-width', newBorderWidth + 'px'
+          );
+          console.log(newBorderWidth);
+            this.updateCSSResult();
         },
 
         //Changing text
       changeText: function() {
             this.preview.text($('#button-text').val());
-            //this.updateHtmlArea();
-        }
+            this.updateHtmlResult();
+        },
 
-      //  //Update result
-      //updateCSSResult: function() {
-      //      var newText = $('#text').val();
-      //      console.log(newText);
-      //      //this.updateResult();
-      //  }
+        //Update result
+      updateCSSResult: function() {
+          var borderRadius = this.preview.css('border-top-left-radius'),
+              borderWidth = this.preview.css('border-width'),
+              cssResult = $('#cssResult');
+
+          cssResult.text(
+              '.button-preview {\n' +
+              'border-width: '          + borderRadius + ';\n' +
+              'border-size: '           + borderWidth + ';\n' +
+              '};'
+          );
+
+        },
+
+      //Динамическое изменение html textarea
+      updateHtmlResult: function(){
+          var htmlCodeResultArea = $('#htmlResult');
+
+          htmlCodeResultArea.text(
+              '<button class="button-preview">'+ $('#button-text').val() +'</button>'
+          );
+      },
+
+      sendForm: function(){
+
+        //Send form start
+          var htmlResult = $('#htmlResult');
+          var cssResult = $('#cssResult');
+          var senderEmail = $('#sender-email');
+
+          var data = 'htmlResult=' + encodeURIComponent(htmlResult.val()) + '&cssResult=' + encodeURIComponent(cssResult.val());
+
+          $.ajax({
+
+              url: "send_email.php",
+              type: "GET",
+              data: data,
+              cache: false,
+
+              success: function(html) {
+
+                  if (html == 1) {
+                      alert('Sent');
+                  } else {
+                      //alert('Sorry, unexpected error. Please try again later22.');
+                  }
+              }
+          });
+
+          return false;
+        //Send form finish
+      }
 
     }
 
